0707dbea42f2d0db58fc72cab4255cc9c09f7c66
jhonaltar <jhon96alta@gmail.com>
2021-06-17 03:44:51 -0500

    Update db.js v1.0.3

--- config/db.js
+++ config/db.js
@@ -5,9 +5,7 @@ module.exports = new Sequelize (process.env.BD_NOMBRE, process.env.BD_USER,proce
     host: process.env.BD_HOST,
     port: process.env.BD_PORT,
     dialect: 'postgres',
-    dialectOptions: {
-        ssl: true
-    },
+    ssl: true,
     pool: {
         max: 5,
         min: 0,
